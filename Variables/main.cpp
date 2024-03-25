783c419a064ad5474919f2cd771c5c9a2fb84989
juanguillermo83 <juanguillermo83@outlook.com>
2024-03-24 21:55:52 -0300

    add string variables globales y funcion

--- Variables/main.cpp
+++ Variables/main.cpp
@@ -2,12 +2,25 @@
 
 using namespace std;
 
+
+void funcion();
+
+string nombreGlobal = "JuanGuillermo";
+
+
 int main()
 {
     int edad;
     edad = 40;
+    int numero {22};  // otra forma de asignar en c++
     string nombre = "Juanito";
     cout << "Mi nombre es " << nombre << " mi edad es " << edad << " años" << endl;
 
+    funcion();
+
     return 0;
+}
+
+void funcion(){
+    cout << "me llamo " << nombreGlobal << " nombre de variable global" << endl;
 }
